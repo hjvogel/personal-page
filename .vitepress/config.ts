f80f99d0a405802ce31bd4c18c324c656976f63c
Holger <Holger@192.168.178.31>
2022-02-19 14:43:07 +0100

    Fix ignore

---
+++ .vitepress/config.ts
@@ -0,0 +1,84 @@
+import { getPosts, getPostLength } from "./theme/serverUtils";
+
+async function config() {
+  return {
+    lang: "en-US",
+    title: "John D",
+    href: "/about/Me",
+    description: "I am John Doe",
+    head: [
+      [
+        "link",
+        {
+          rel: "icon",
+          // type: 'image/png',
+          type: "image/jpeg",
+          //link: "/about/Me",
+        },
+      ],
+      [
+        "meta",
+        {
+          name: "author",
+          content: "John Doe",
+href: "/about/Me",
+        },
+      ],
+      [
+        "meta",
+        {
+          property: "og:title",
+          content: "Home",
+        },
+      ],
+      [
+        "meta",
+        {
+          property: "og:description",
+          content: "Home of John Doe",
+        },
+      ],
+    ],
+    themeConfig: {
+      //repo: "my/GithubSite", moved into about
+      logo: "/tea.svg",
+      docsDir: "/",
+      // docsBranch: "master",
+      lastUpdated: false,
+      posts: await getPosts(),
+      pageSize: 3,
+      postLength: await getPostLength(),
+
+      //       algolia: {  search tool
+      //         apiKey: "xxx",
+      //         indexName: "my-docs",
+      //       },
+
+      nav: [  // TODO menu or toggle & hide options of side shown (no link needed to where we are)
+        {
+          text: "Home",
+          link: "/",
+        },
+        {
+          text: "Tags",
+          link: "/tags",
+        },
+        {
+          text: "Hist",
+          link: "/archives",
+        },
+	      {  // move up
+          text: "About",
+	link: "/about/Me",
+	      },        
+      ],
+
+      // sidebar: {
+      //   "./posts/": false,
+      //   "/": false,
+      // },
+      sidebar: false,
+    },
+  };
+}
+export default config();
