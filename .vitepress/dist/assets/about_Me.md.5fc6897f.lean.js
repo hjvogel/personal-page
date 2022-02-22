4fffbac22059767bf6c9f882ebe0088e6a8598bb
Holger <Holger@192.168.178.31>
2022-02-22 14:58:01 +0100

    try dist

---
+++ .vitepress/dist/assets/about_Me.md.5fc6897f.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as a,o as t,a as n}from"./app.7239ea75.js";const u='{"title":"About Me","description":"","frontmatter":{},"headers":[{"level":3,"title":"About Me","slug":"about-me"}],"relativePath":"about/Me.md","lastUpdated":1645258359790}',o={},r=n("",6),i=[r];function s(l,d,c,h,g,_){return t(),a("div",null,i)}var m=e(o,[["render",s]]);export{u as __pageData,m as default};
