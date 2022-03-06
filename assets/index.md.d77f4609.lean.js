1c6fb8f4e74d07637f36d78123b216a0f167642e
hjvogel <hjvogel@users.noreply.github.com>
2022-03-06 16:13:24 +0000

    deploy: e3ca9406ff4e96cf9d569a30000b053449746081

--- assets/index.md.d77f4609.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as a}from"./app.4b3bdaac.js";const l='{"title":"home","description":"","frontmatter":{"home":true,"date":"2022-02-14T00:00:00.000Z","title":"home","sidebar":false},"headers":[],"relativePath":"index.md","lastUpdated":1646583101304}',r={};function o(s,n,c,d,i,p){return a(),t("div")}var m=e(r,[["render",o]]);export{l as __pageData,m as default};
